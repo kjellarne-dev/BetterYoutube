2074ce83d054d2bc2a94ffbfb5f3b03bbb1c074f
Kjellarne6 <Kjellarne6@users.noreply.github.com>
2020-04-30 03:21:41 +0200

    Add files via upload

---
+++ Greasemonkey-scripts/remove-youtubeminiplayer.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name     _Remove annoying Youtube Miniplayer
+// @include  https://www.youtube.com/*
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @grant    GM_addStyle
+// ==/UserScript==
+//- The @grant directive is needed to restore the proper sandbox.
+
+/*--- Use the jQuery contains selector to find content to remove.
+    Beware that not all whitespace is as it appears.
+*/
+
+/*
+	Remove miniplayer HTML code
+*/
+
+var miniplayer = document.getElementsByClassName('miniplayer style-scope ytd-miniplayer')[0].parentElement;
+
+if (miniplayer) {
+  miniplayer.parentNode.removeChild(miniplayer);
+}
